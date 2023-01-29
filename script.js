8f6f5ca995bda2a73db2aaf6dffaee95ca3790f7
Danis Syrtlanov <danissyrtlanov@gmail.com>
2023-01-29 15:17:18 +0300

    add script in HW

---
+++ script.js
@@ -0,0 +1,80 @@
+// Задание 1
+
+// let password = 'пароль';
+// let input = String(prompt('enter password'));
+
+// input === password ? console.log('correct password') : console.log('wrong password');
+
+// Задание 2 
+
+// let c = Number(prompt('enter number'));
+
+// c >= 0 && c <= 10 ? console.log('correct') : console.log('wrong');;
+
+// Задание 3 
+
+// let d = Number(prompt('first number'));
+// let e = Number(prompt('second number'));
+
+// if (d > 100 || e > 100) {
+//     console.log('cool');
+// } else {
+//     console.log('not cool');
+// }
+
+// Задание 4 
+
+// let a = '2';
+// let b = '3';
+
+// a = Number(a);
+// b = Number(b);
+
+// alert(a + b);
+
+// Задание 5 
+
+// let monthNumber = Number(prompt('Enter the number of month'));
+
+// switch (monthNumber) {
+//     case 1:
+//         console.log('winter');
+//         break;
+//     case 2:
+//         console.log('winter');
+//         break;
+//     case 12:
+//         console.log('winter');
+//         break;
+//     case 3:
+//         console.log('spring');
+//         break;
+//     case 4:
+//         console.log('spring');
+//         break;
+//     case 5:
+//         console.log('spring');
+//         break;
+//     case 6:
+//         console.log('summer');
+//         break;
+//     case 7:
+//         console.log('summer');
+//         break;
+//     case 8:
+//         console.log('summer');
+//         break;
+//     case 9:
+//         console.log('autumn');
+//         break;
+//     case 10:
+//         console.log('autumn');
+//         break;
+//     case 11:
+//         console.log('autumn');
+//         break;
+//     default:
+//         console.log('wrong number');
+//         break;
+// }
+
